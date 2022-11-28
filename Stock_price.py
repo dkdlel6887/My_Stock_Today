e131e1b4a8ab88a58511ce2a86875f96ad9cbf11
Yeonghan Kim <dkdlel6887@naver.com>
2022-11-28 16:24:29 +0900

    feat: 6.Stock price 손익률 데이터 추가

--- Stock_price.py
+++ Stock_price.py
@@ -15,7 +15,7 @@ url = r'https://finance.naver.com/'
 wd.get(url)
 time.sleep(3)
 
-exprice_lst = []  # 전일 종가 정보 저장 *11.28 03:36 기준 2일전 가격 책정이지만 오전에 장시작전 주가 변동없는거 확인되면 현재가격으로 변경
+exprice_lst = []  # 전일 종가 정보 저장(오전 장시작 전 기준: 현재가->전일 종가)
 
 stock_data = list(pd.read_excel('user_infos.xlsx', engine='openpyxl', header=0, index_col=0)['종목명'][:])
 #stock_data = ['두산에너빌리티', '카카오', '삼성전자', 'lg에너지솔루션']
@@ -25,11 +25,12 @@ for i in stock_data:
     time.sleep(1)
     wd.find_element(By.XPATH, '/html/body/div[2]/div[1]/div/div[2]/form/fieldset/div/div[2]/div/div[1]/div/div/ul/li/a').click()
     time.sleep(1)
-
+#'/html/body/div[3]/div[2]/div[2]/div[1]/div[1]/div[1]/div/p[1]/em/span'
     exprice = wd.find_elements(By.XPATH,'/html/body/div[3]/div[2]/div[2]/div[1]/div[1]/div[1]/div/p[1]/em/span')
     for j in range(len(exprice)):
         exprice[j] = exprice[j].text
     exprice_lst.append(int(''.join(exprice).replace(',', "")))
+print(exprice_lst)
 
 fpath = 'user_infos.xlsx'
 wb = openpyxl.load_workbook(fpath)
@@ -37,8 +38,13 @@ ws = wb['user1']
 ws['F1'] = '전일 종가'
 ws['G1'] = '평가 금액'
 ws['H1'] = '평가 손익'
-for k in range(2,len(stock_data)+2):
+ws['I1'] = '손익률'
+for k in range(2, len(stock_data)+2):
     ws[f'F{k}'] = exprice_lst[k-2]
     ws[f'G{k}'] = int(ws[f'F{k}'].value) * int(ws[f'D{k}'].value)  # 평가금액 = 전일종가 * 보유수량
     ws[f'H{k}'] = int(ws[f'G{k}'].value) - int(ws[f'E{k}'].value)  # 평가손익 = 평가금액 - 매수금액
+    if int(ws[f'E{k}'].value) != 0:
+        ws[f'I{k}'] = int(ws[f'H{k}'].value) / int(ws[f'E{k}'].value) * 100  # 손익률 = 평가손익 / 매수금액
+    else:
+        ws[f'I{k}'] = 0
 wb.save(fpath)
